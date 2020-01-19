f7db1ae5b6bd891d4e5ed293f9ae3407d26cf7db
JVK1890728 <jay.vachhani@outlook.com>
2020-01-19 19:38:39 +0000

    Add Excersise 1 completed, Initial folder structure

---
+++ Data Analytics/le-wagon-exercises/playground/playground.py
@@ -0,0 +1,13 @@
+# TODO: it's a playground, let's write some code (no unit tests to run here)
+pi = 3.14
+radius = 5
+r = f'The radius is set to {radius}'
+print(r)
+
+perimeter = 2 * pi * radius
+p = f'Perimeter of the circle is {round(perimeter,1)}'
+print(p)
+
+area = pi * (radius ** 2)
+a = f'Area of the disk is {area}'
+print(a)
