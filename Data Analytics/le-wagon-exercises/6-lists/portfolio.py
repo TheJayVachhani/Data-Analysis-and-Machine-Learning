fb21d8211cd5f65d9a0bd37f36231c753d1311f3
JVK1890728 <jay.vachhani@outlook.com>
2020-01-19 22:17:40 +0000

    Complete Portfolio Excercise

--- Data Analytics/le-wagon-exercises/6-lists/portfolio.py
+++ Data Analytics/le-wagon-exercises/6-lists/portfolio.py
@@ -6,3 +6,15 @@ tsla = [ 12, 342.12 ]
 fb   = [ 18, 209.0  ]
 
 portfolio = [ aapl, goog, tsla, fb ]
+fbs = portfolio[3][0]
+
+market = [ 198.84, 1217.93, 267.66, 179.06 ]
+# pnl function designed to take portfolio and market
+# where they are the same length and return overall performance
+def pnl(pfl, mkt):
+    result = []
+    for n, value in enumerate(mkt):
+        result.append(pfl[n][0] * (value - pfl[n][1]))
+    product = sum(float(n) for n in result)
+    return product
+print (pnl(portfolio, market))
