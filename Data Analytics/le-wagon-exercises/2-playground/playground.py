9733566abd48329166c6e57d82cafb1384bcf955
JVK1890728 <jay.vachhani@outlook.com>
2020-01-19 21:16:06 +0000

    Refactor playground.py as instructed in Function Lecture

--- Data Analytics/le-wagon-exercises/2-playground/playground.py
+++ Data Analytics/le-wagon-exercises/2-playground/playground.py
@@ -1,13 +1,32 @@
 # TODO: it's a playground, let's write some code (no unit tests to run here)
+# New playground from Function
+import math
 pi = 3.14
-radius = 5
-r = f'The radius is set to {radius}'
-print(r)
 
-perimeter = 2 * pi * radius
-p = f'Perimeter of the circle is {round(perimeter,1)}'
-print(p)
+def circle_math(radius):
+    result = []
+    result.append(pi * 2 * radius)
+    result.append(pi * radius ** 2)
+    result.sort()
+    return result
 
-area = pi * (radius ** 2)
-a = f'Area of the disk is {area}'
-print(a)
+## Test Cases for circle_math method
+#values = circle_math(5)
+#print(f"Radius=5 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")
+
+#values = circle_math(6)
+#print(f"Radius=6 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")
+
+# Old Playground.py before Function
+#pi = 3.14
+#radius = 5
+#r = f'The radius is set to {radius}'
+#print(r)
+
+#perimeter = 2 * pi * radius
+#p = f'Perimeter of the circle is {round(perimeter,1)}'
+#print(p)
+
+#area = pi * (radius ** 2)
+#a = f'Area of the disk is {area}'
+#print(a)
