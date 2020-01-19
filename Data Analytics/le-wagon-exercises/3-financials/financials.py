cc3670c740d5767a234bd95603dcd5b967e400d3
JVK1890728 <jay.vachhani@outlook.com>
2020-01-19 20:43:23 +0000

    Complete excerise 3, add gitignore file

--- Data Analytics/le-wagon-exercises/3-financials/financials.py
+++ Data Analytics/le-wagon-exercises/3-financials/financials.py
@@ -1,5 +1,17 @@
 # pylint: disable=missing-docstring
 
 # TODO: 1st exercise: Define the `forward_price` function
+import math
+from math import e
+
+def forward_price(spot, interest_rate, time):
+    return round(spot * (e **(interest_rate * time)), 2)
 
 # TODO: 2nd exercise: Define the `short_pnl` function
+# sum of  all (position - mtm)
+# both lists the same length
+def short_pnl(positions, mtm):
+    total_positions = sum(int(position) for position in positions)
+    total_m = sum(int(m) for m in mtm)
+    result = total_positions - total_m
+    return result
