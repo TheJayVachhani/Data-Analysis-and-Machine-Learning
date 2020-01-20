8c6b9894db420944c23b38fe903d8e4366bdd7d1
JVK1890728 <jay.vachhani@outlook.com>
2020-01-20 16:36:15 +0000

    Fix Folders and add commas to portfolio dictionary

--- Data Analytics/le-wagon-exercises/8-dictionaries/portfolio_advanced.py
+++ Data Analytics/le-wagon-exercises/8-dictionaries/portfolio_advanced.py
@@ -5,15 +5,15 @@ portfolio ={
     "AAPL" : {
         "quantity" : 10,
         "strike" : 154.12
-    }
+    },
     "GOOG" : {
         "quantity" : 2,
         "strike" : 812.56
-    }
+    },
     "TSLA" : {
         "quantity" : 12,
         "strike" : 342.12        
-    }
+    },
     "FB" : {
         "quantity" : 18,
         "strike" : 209.0
