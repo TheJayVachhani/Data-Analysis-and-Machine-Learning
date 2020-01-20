7d10ec307644e0d53dd3361560a2e3e1627bf33f
JVK1890728 <jay.vachhani@outlook.com>
2020-01-20 16:29:59 +0000

    Change original portfolio format from list to dictionary

--- Data Analytics/le-wagon-exercises/8-dictionaries/portfolio_advanced.py
+++ Data Analytics/le-wagon-exercises/8-dictionaries/portfolio_advanced.py
@@ -1,3 +1,21 @@
 # pylint: disable=missing-docstring
 
 # TODO: start by defining a `portfolio` using a dict!
+portfolio ={
+    "AAPL" : {
+        "quantity" : 10,
+        "strike" : 154.12
+    }
+    "GOOG" : {
+        "quantity" : 2,
+        "strike" : 812.56
+    }
+    "TSLA" : {
+        "quantity" : 12,
+        "strike" : 342.12        
+    }
+    "FB" : {
+        "quantity" : 18,
+        "strike" : 209.0
+    }
+}
