3968c50cd24b2e7a54d6a07e54230116d9d3ba62
JVK1890728 <jay.vachhani@outlook.com>
2020-02-06 17:53:24 +0000

    Add work for boolean indexing

--- Data Analytics/le-wagon-exercises/6-lists/portfolio.py
+++ Data Analytics/le-wagon-exercises/6-lists/portfolio.py
@@ -5,7 +5,7 @@ goog = [  2, 812.56 ]
 tsla = [ 12, 342.12 ]
 fb   = [ 18, 209.0  ]
 
-portfolio = [ aapl, goog, tsla, fb ]
+portfolio = [ aapl, goog, tsla, fb ] 
 fbs = portfolio[3][0]
 
 market = [ 198.84, 1217.93, 267.66, 179.06 ]
